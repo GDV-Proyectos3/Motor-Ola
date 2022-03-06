2b4281d8d16c9bb86d833735656f1fe306725126
Oscar <oscarped@ucm.es>
2022-03-06 02:12:49 +0100

    Integrado SDL en motor

--- MotorOla_Solution/Src/MotorOla/Motor.cpp
+++ MotorOla_Solution/Src/MotorOla/Motor.cpp
@@ -13,6 +13,9 @@ Motor::Motor()
 Motor::~Motor()
 {
 	//Destruye los singletons
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	
 }
 
@@ -20,12 +23,31 @@ void Motor::initSystems()
 {
 	//Inicializa a cada uno de los singletons
 	//p->init();
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("Prueba SDL", SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, WINWIDTH, WINHEIGHT, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+void Motor::handleEvents() {
+	SDL_Event event;
+	while (SDL_PollEvent(&event) && !stop) {
+		if (event.type==SDL_QUIT||event.type==SDL_KEYDOWN && event.key.keysym.sym==SDLK_ESCAPE) {
+			stop = true;
+		}
+		else {
+			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_1) {
+				std::cout << "Bucle" << std::endl;
+			}
+		}
+
+	}
 }
 
 void Motor::updateSystems()
 {
 	//Actualiza el motor. Bucle input->update/fisicas->render
 	while (!stop) {
+		handleEvents();
 		//Input
 		//Update
 		//Render
--- MotorOla_Solution/Src/MotorOla/Motor.h
+++ MotorOla_Solution/Src/MotorOla/Motor.h
@@ -1,14 +1,21 @@
 #pragma once
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <iostream>
 
 class Motor
 {
 private:
-	
+	SDL_Window* window;
+	const int WINWIDTH = 800;
+	const int WINHEIGHT = 600;
+	SDL_Renderer* renderer;
 	//Ogre
 	//Physx
 	//FMod
 	//Cegui
 	bool stop = false;
+	void handleEvents();
 
 public:
 	Motor();
