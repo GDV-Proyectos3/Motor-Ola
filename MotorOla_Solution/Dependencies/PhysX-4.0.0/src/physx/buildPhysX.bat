9b70dbc521d48192dc451b4af64b80e0b5f41bdd
ACE D. POL <pablocub@ucm.es>
2022-03-11 14:55:07 +0100

    Actualización: Physx SDK 4.0 ---> PhysX SDK 4.1 :warning:

--- MotorOla_Solution/Dependencies/PhysX-4.0.0/src/physx/buildPhysX.bat
+++
@@ -1,9 +0,0 @@
-mkdir ..\..\build\public
-copy compiler\public\CMakeLists.txt ..\..\build\public\CMakeLists.txt
-::::::::rmdir /q /s "..\..\build\vc16win64"
-::https://stackoverflow.com/questions/1103994/how-to-run-multiple-bat-files-within-a-bat-file
-call generate_projects.bat
-cd compiler
-::::::::move vc16win64 ..\..\..\build\vc16win64
-set /p DUMMY=--Hecho--
-exit
