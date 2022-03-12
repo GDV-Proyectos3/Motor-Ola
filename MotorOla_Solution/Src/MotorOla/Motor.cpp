b2c7f5674acafe92ebfc92bca92a2fd18be43d98
Oscar <oscarped@ucm.es>
2022-03-12 01:24:40 +0100

    Cambio en el handleEvents del motor

--- MotorOla_Solution/Src/MotorOla/Motor.cpp
+++ MotorOla_Solution/Src/MotorOla/Motor.cpp
@@ -52,13 +52,19 @@ void Motor::handleEvents()
 	SDL_Event event;
 	while (SDL_PollEvent(&event) && !stop) {
 
-		if (event.type == SDL_QUIT||event.type==SDL_KEYDOWN && event.key.keysym.sym==SDLK_ESCAPE) {
+		if (event.type == SDL_QUIT) {
 
 			stop = true;
 		}
-		else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_0) {
-			std::cout << "Input" << std::endl;
+		if (event.type == SDL_KEYDOWN) {
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				stop = true;
+			}
+			else if (event.key.keysym.sym == SDLK_0) {
+				std::cout << "Input" << std::endl;
+			}
 		}
+		
 		
 	}
 }
