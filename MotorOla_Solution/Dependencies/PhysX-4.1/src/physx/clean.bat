2c01dfdfbe47b6f0a76bbb5180e6f8c499799edf
ACE D. POL <pablocub@ucm.es>
2022-03-17 03:16:55 +0100

    Create clean.bat

---
+++ MotorOla_Solution/Dependencies/PhysX-4.1/src/physx/clean.bat
@@ -0,0 +1,52 @@
+@REM Elimina todos los ficheros y directorios generados
+@REM que quedan excluidos del repositorio.
+
+del /q include\PxConfig.h
+
+@REM \bin
+cd bin\win.x86_64.vc142.mt\
+mkdir temporal
+
+cd release
+move glut32.dll ..\temporal
+move PhysXDevice64.dll ..\temporal
+del /s /q *.*
+cd ..\temporal
+move glut32.dll ..\release
+move PhysXDevice64.dll ..\release
+cd ..
+
+cd debug
+move glut32.dll ..\temporal
+move PhysXDevice64.dll ..\temporal
+del /s /q *.*
+cd ..\temporal
+move glut32.dll ..\debug
+move PhysXDevice64.dll ..\debug
+cd ..
+
+cd profile
+move glut32.dll ..\temporal
+move PhysXDevice64.dll ..\temporal
+del /s /q *.*
+cd ..\temporal
+move glut32.dll ..\profile
+move PhysXDevice64.dll ..\profile
+cd ..
+
+cd checked
+move glut32.dll ..\temporal
+move PhysXDevice64.dll ..\temporal
+del /s /q *.*
+cd ..\temporal
+move glut32.dll ..\checked
+move PhysXDevice64.dll ..\checked
+cd ..
+
+rmdir /s /q temporal
+cd ..\..
+
+@REM \build
+rmdir /s /q ..\..\build\vc16win64\.vs
+rmdir /s /q ..\..\build\vc16win64\x64
+rmdir /s /q ..\..\build
