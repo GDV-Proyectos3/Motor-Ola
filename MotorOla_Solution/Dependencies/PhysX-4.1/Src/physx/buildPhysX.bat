44248ae1c2ea129bb63f8a6770ac09792187cdfb
Javier Rubio <jrubio05@ucm.es>
2022-03-20 18:44:50 +0100

    fallo automatizacion script

--- MotorOla_Solution/Dependencies/PhysX-4.1/Src/physx/buildPhysX.bat
+++ MotorOla_Solution/Dependencies/PhysX-4.1/Src/physx/buildPhysX.bat
@@ -1,7 +1,7 @@
 mkdir ..\..\Build\public
 copy compiler\public\CMakeLists.txt ..\..\Build\public\CMakeLists.txt
-xcopy /E /I samples\media ..\..\Build\samples\media
-xcopy /E /I samples\sampleframework\media ..\..\Build\sampleframework\media
+xcopy /E /I /Y samples\media ..\..\Build\samples\media
+xcopy /E /I /Y samples\sampleframework\media ..\..\Build\sampleframework\media
 call generate_projects.bat
 set /p DUMMY=--Hecho--
 exit
