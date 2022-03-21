552aefd0fe542c9252c3aac0031b28c677ba9d98
Oscar <oscarped@ucm.es>
2022-03-21 23:05:34 +0100

    Descomentado FMod

--- MotorOla_Solution/Src/MotorOla/FMODAudioManager.h
+++ MotorOla_Solution/Src/MotorOla/FMODAudioManager.h
@@ -1,31 +1,31 @@
-//#pragma once
-//
-//#include "AudioManager.h"
-//#include <fmod.hpp>
-//#include <fmod_errors.h>
-//#include <map>
-//
-//class FMODAudioManager : public AudioManager {
-//public:
-//	FMODAudioManager();
-//	~FMODAudioManager();
-//
-//	void init() override;
-//	void update() override;
-//
-//	virtual void loadMusic(int channel, const char* fileName)override;
-//	virtual void playMusic(int channel, bool loops ) override;
-//	virtual void stopMusic(int channel) override;
-//	virtual void togglePause(int channel) override;
-//	virtual void setVolume(int chan, int volume) override;
-//	virtual void setPitch(int chan, int pitch) override;
-//	virtual void fadeIn(int chan) override;
-//	virtual void fadeOut(int chan)override;
-//	void checkError(FMOD_RESULT result ) ;
-//
-//private:
-//	FMOD::System* system;
-//	FMOD_RESULT result;
-//	FMOD::Channel* channel[24];
-//	FMOD::Sound* sound[24];
-//};
+#pragma once
+
+#include "AudioManager.h"
+#include <fmod.hpp>
+#include <fmod_errors.h>
+#include <map>
+
+class FMODAudioManager : public AudioManager {
+public:
+	FMODAudioManager();
+	~FMODAudioManager();
+
+	void init() override;
+	void update() override;
+
+	virtual void loadMusic(int channel, const char* fileName)override;
+	virtual void playMusic(int channel, bool loops ) override;
+	virtual void stopMusic(int channel) override;
+	virtual void togglePause(int channel) override;
+	virtual void setVolume(int chan, int volume) override;
+	virtual void setPitch(int chan, int pitch) override;
+	virtual void fadeIn(int chan) override;
+	virtual void fadeOut(int chan)override;
+	void checkError(FMOD_RESULT result ) ;
+
+private:
+	FMOD::System* system;
+	FMOD_RESULT result;
+	FMOD::Channel* channel[24];
+	FMOD::Sound* sound[24];
+};
