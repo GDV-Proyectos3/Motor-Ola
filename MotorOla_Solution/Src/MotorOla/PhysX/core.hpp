e5764dafb7bdcffa9422acadacd8be7caaafb67e
ACE D. POL <pablocub@ucm.es>
2022-03-24 14:15:52 +0100

    Prueba de concepto --> ( helloPhysX )

---
+++ MotorOla_Solution/Src/MotorOla/PhysX/core.hpp
@@ -0,0 +1,19 @@
+#ifndef __CORE_HPP__
+#define __CORE_HPP__
+
+#include "Common/Print.h"
+#include "Common/PVD.h"
+#include "Utils/Utils.h"
+
+#include "Render/Render.h"
+#include "Render/Camera.h"
+
+#include <vector>
+
+using namespace Snippets;
+
+typedef physx::PxVec3 Vector3;
+typedef physx::PxVec4 Vector4;
+
+
+#endif
