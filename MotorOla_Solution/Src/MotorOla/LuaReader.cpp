1778237a51b381fe30cbbad2ac4312b63b13db2f
LiyliLee <45203984+LiyliLee@users.noreply.github.com>
2022-04-01 11:09:01 +0200

    pasar a lo de h a cpp

--- MotorOla_Solution/Src/MotorOla/LuaReader.cpp
+++ MotorOla_Solution/Src/MotorOla/LuaReader.cpp
@@ -1,5 +1,6 @@
 #include "LuaReader.h"
 #include <iostream>
+
 extern "C"
 {
     
@@ -103,4 +104,37 @@ inline T LuaScript::get(const std::string& variableName)
 
     lua_pop(L, level + 1); // pop all existing elements from stack
     return result;
+}
+
+template<>
+inline std::string LuaScript::lua_getdefault() {
+    return "null";
+}
+
+template <>
+inline float LuaScript::lua_get(const std::string& variableName) {
+    if (!lua_isnumber(L, -1)) {
+        printError(variableName, "Not a number");
+    }
+    return (float)lua_tonumber(L, -1);
+}
+
+template <>
+inline int LuaScript::lua_get(const std::string& variableName) {
+    if (!lua_isnumber(L, -1)) {
+        printError(variableName, "Not a number");
+    }
+    return (int)lua_tonumber(L, -1);
+}
+
+template <>
+inline std::string LuaScript::lua_get(const std::string& variableName) {
+    std::string s = "null";
+    if (lua_isstring(L, -1)) {
+        s = std::string(lua_tostring(L, -1));
+    }
+    else {
+        printError(variableName, "Not a string");
+    }
+    return s;
 }
