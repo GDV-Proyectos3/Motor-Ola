d126f62b8e60fd5061c9e35a69b6884a1ebc7b55
OskarFreestyle <oscafe01@ucm.es>
2022-04-20 19:56:53 +0200

    Dejo subida la escena de testeo para que esté en el repo

---
+++ MotorOla_Solution/Exes/Assets/Scenes/TestScene.lua
@@ -0,0 +1,50 @@
+local level = {
+    backgroundColor="0.6,1.0,0.0",
+    ambient="0.1,0.1,0.1",
+    gravity="0 -9.8 0",
+    entidades = {
+        {
+            name="Pala1",
+            id=1,
+            components={
+                transform={
+                    parent="-1",
+                    position="100,10,0",
+                    rotation="0,0,0",
+                    scale="5,5,5"
+                },
+                mesh={
+                    mesh="ogrehead.mesh",
+                    material="",
+                    visible="true"
+                }
+            }
+        },
+        {
+            name="Pala2",
+            id=2,
+            components={
+                transform={
+                    parent="-1",
+                    position="0,0,0",
+                    rotation="0,0,0",
+                    scale="1,1,1"
+                },
+                mesh={
+                    mesh="cube.mesh",
+                    material="Rojo",
+                    visible="true"
+                }
+            }
+        }
+    }
+}
+
+function GetLevel ()
+  return level
+end
+
+function pruebaTexto()
+    print('Im Working YESS')
+end
+
---
+++ MotorOla_Solution/Exes/Assets/Scenes/run.lua
@@ -0,0 +1,14 @@
+level1 =
+{
+  music = "music1",
+
+  items =
+  {
+    A = 1,
+    B = 2,
+
+    {
+      c = "C",
+    }
+  }
+}
