02b83f3957d7fa0a0d357acc9c3ae66b6815766f
LiyliLee <45203984+LiyliLee@users.noreply.github.com>
2022-04-22 09:37:25 +0200

    Quitar lo que no funcional del luaReader

--- MotorOla_Solution/Src/MotorOla/LuaReader.cpp
+++ MotorOla_Solution/Src/MotorOla/LuaReader.cpp
@@ -1,11 +1,6 @@
 #include "LuaReader.h"
 
-#include <iostream>
 #include <vector>
-//
-//#include <lua.hpp>
-//#include <lauxlib.h>
-//#include <lualib.h>
 #include <iostream>
 #include <map>
 
@@ -23,17 +18,6 @@ extern "C"
     #include "lualib.h"
 }
 
-LuaScript::LuaScript(const std::string&  filename)
-{
-	L = luaL_newstate();
-	luaL_openlibs(L);
-	if (luaL_loadfile(L, filename.c_str()) || lua_pcall(L, 0, 0, 0))
-	{
-		std::cout << "Error: script not loaded (" << filename << ")" << std::endl;
-		L = 0;
-	}
-		
-}
 
 static const luaL_Reg lualibs[] = {
 		{ "base",       luaopen_base },
@@ -49,123 +33,6 @@ void openlualibs(lua_State* l) {
 	}
 }
 
-LuaScript::~LuaScript()
-{
-	if (L) 
-		lua_close(L);
-}
-
-void LuaScript::printError(const std::string& variableName, const std::string& reason)
-{
-	std::cout << "Error: can't get [" << variableName << "]. " << reason << std::endl;
-}
-
-bool LuaScript::lua_gettostack(const std::string& variableName)
-{
-    level = 0;
-    std::string var = "";
-    for (unsigned int i = 0; i < variableName.size(); i++) {
-        if (variableName.at(i) == '.') {
-            if (level == 0) {
-                lua_getglobal(L, var.c_str());
-            }
-            else {
-                lua_getfield(L, -1, var.c_str());
-            }
-
-            if (lua_isnil(L, -1)) {
-                printError(variableName, var + " is not defined");
-                return false;
-            }
-            else {
-                var = "";
-                level++;
-            }
-        }
-        else {
-            var += variableName.at(i);
-        }
-    }
-    if (level == 0) {
-        lua_getglobal(L, var.c_str());
-    }
-    else {
-        lua_getfield(L, -1, var.c_str());
-    }
-    if (lua_isnil(L, -1)) {
-        printError(variableName, var + " is not defined");
-        return false;
-    }
-
-    return true;
-}
-
-template<typename ...Targs>
-void LuaScript::callFunction(std::string functionName,int nArg, Targs ...args)
-{
-    lua_getglobal(L, functionName.c_str());
-    for (int i = 0; i < nArg; i++)
-    {
-        lua_pushnumber(L, args[i]);
-    }
-    if (lua_pcall(L, nArg, 1, 0) != 0)
-        printError(functionName, "function is not defined");
-
-}
-
-template<typename T>
-inline T LuaScript::get(const std::string& variableName)
-{
-	if (!L) {
-		printError(variableName, "Script is not loaded");
-		return lua_getdefault<T>();
-	}
-
-	T result;
-
-    if (lua_gettostack(variableName)) { // variable succesfully on top of stack
-        result = lua_get<T>(variableName);
-    }
-    else {
-        result = lua_getdefault<T>();
-    }
-
-    lua_pop(L, level + 1); // pop all existing elements from stack
-    return result;
-}
-
-template<>
-inline std::string LuaScript::lua_getdefault() {
-    return "null";
-}
-
-template <>
-inline float LuaScript::lua_get(const std::string& variableName) {
-    if (!lua_isnumber(L, -1)) {
-        printError(variableName, "Not a number");
-    }
-    return (float)lua_tonumber(L, -1);
-}
-
-template <>
-inline int LuaScript::lua_get(const std::string& variableName) {
-    if (!lua_isnumber(L, -1)) {
-        printError(variableName, "Not a number");
-    }
-    return (int)lua_tonumber(L, -1);
-}
-
-template <>
-inline std::string LuaScript::lua_get(const std::string& variableName) {
-    std::string s = "null";
-    if (lua_isstring(L, -1)) {
-        s = std::string(lua_tostring(L, -1));
-    }
-    else {
-        printError(variableName, "Not a string");
-    }
-    return s;
-}
 
 void readFile(std::string file) {
 	// Vector de entidades que creamos y vector auxiliar para marcarlas iniciadas
--- MotorOla_Solution/Src/MotorOla/LuaReader.h
+++ MotorOla_Solution/Src/MotorOla/LuaReader.h
@@ -3,34 +3,12 @@
 extern "C"
 {
     struct lua_State;
-    /*
-    #include "lua.h"
-    #include "lauxlib.h"
-    #include "lualib.h"
-    */
+   
 }
 
 
 
 
-/*
-* 
-player = {
-    pos = {
-         X = 20,
-         Y = 30,
-    },
-    filename = "res/images/player.png",
-    HP = 20,
--- you can also have comments
-}
-With a little class(implementation is below) you can get data in this way:
-
-LuaScript script("player.lua");
-std::string filename = script.get("player.filename");
-int posX = script.get("player.pos.X");
-
-*/
 
 void readFile(std::string file);
 
@@ -40,41 +18,5 @@ void readFileTest(std::string file);
 
 class Entidad;
 Entidad* readPrefab(std::string file);
-
-class LuaScript
-{
-public:
-	LuaScript(const std::string&  filename);
-	~LuaScript();
-
-	void printError(const std::string& variableName, const std::string& reason);
-
-    template<typename... Targs>
-    void callFunction(std::string functionName , int nArg, Targs... args);
-
-    template<typename T>
-    T get(const std::string& variableName);
-
-    bool lua_gettostack(const std::string& variableName);
-
-    // Generic get
-    template<typename T>
-    T lua_get(const std::string& variableName) {
-        return 0;
-    }
-
-
-    // Generic default get
-    template<typename T>
-    T lua_getdefault() {
-        return 0;
-    }
-   
-
-private:
-    lua_State* L;
-    int level = 0;
-
-};
 
 
